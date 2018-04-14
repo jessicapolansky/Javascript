e1ce874d5aa502f743feabdb8151ab27415b5947
Jessica Robinson <jessica.polansky@gmail.com>
2018-04-14 21:08:58 +0000

    Completed Javascript Session 1 HW

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
+// Madlib. Enter a name and a subject, and generate a sentence
 function madlib (name, subject) {
     var stringPrint = name + "'s favorite class is " + subject + ".";
     return stringPrint;
 };
 madlib ("Kerry", "math");
 
+// Tip Amount. Enter your bill amount and quality of service to get the tip
 function tipAmount (bill, service) {
     if (service == 'good') {
         var tip = bill * .20;
@@ -23,7 +25,7 @@ function tipAmount (bill, service) {
 }
 tipAmount(20.23, 'fair');
 
-
+// Total Amount. Enter your bill amount and quality of service to get your total
 function totalAmount (bill, service) {
     var total = Number(tipAmount(bill, service)) + bill;
     return total;
@@ -40,7 +42,7 @@ var count = function (start, end) {
     }
       
     };
-    
+// Print a square made out of stars    
 var row = 1;    
 var squareStar = function (size) {
     if (row <= size) {
@@ -52,7 +54,7 @@ var squareStar = function (size) {
     return;   
     }
 };
-
+// Print an empty square outlined by stars    
 function printBox(width, height) {
     console.log(Array(width + 1).join("*"));
 for (var count=0; count < (height - 2); count++) {
@@ -61,18 +63,18 @@ for (var count=0; count < (height - 2); count++) {
     console.log(Array(width + 1).join("*"));
 
 }
-
+// Print a phrase outlined by stars, like a banner   
 function printBanner(text) {
     var width = text.length;
     console.log(Array(width + 3).join("*"));
     console.log("*" + text + "*");
     console.log(Array(width + 3).join("*"));
 }
-
+// Given a phrase, translate into 'leetSpeak'
 function leetSpeak(entry) {
     var upper = entry.toUpperCase();
     var input = upper.split("");
-    var leet = []
+    var leet = [];
     for (var count = 0; count < input.length; count++) {
         if (input[count] == "A") {
             leet.push("4");
@@ -94,4 +96,70 @@ function leetSpeak(entry) {
     }
     var answer = leet.join('');
     console.log(answer);
+}
+// Given a word or phrase, elongate the vowels in printed statement
+function elongate(entry) {
+    var long = [];
+    for (var x = 0; x < entry.length; x++) {
+        if (x + 1 == entry.length) {
+            long.push(entry[x]);
+        }
+        else if (entry[x] + entry[x + 1] == "aa" || entry[x] + entry[x + 1] == "ee" || entry[x] + entry[x + 1] == "oo") {
+            long.push(entry[x] + entry[x+1] + entry[x]  + entry[x]);
+        }
+        else {
+            long.push(entry[x]);
+        }
+    }
+    var answer = long.join('');
+    console.log(answer);
+}
+
+// Given an array of numbers, only return the postive ones!
+var numbers = [-5, 2, -7, 3, 4, -10, 8];
+numbers.filter(function (item) {
+    return item >= 0;
+});
+
+// Caesar Cipher
+function caesarCipher(msg) {
+    //create an array of characters from the English alphabet to be used to determine cipher values
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+    //create a store to keep our response
+    let answer = '';
+    for (let x = 0; x < msg.length; x++) {
+        const chr = msg[x];
+        //When a space is present we wish to ignore it and not invoke cipher logic
+        if (chr == ' ') {
+            answer += ' ';
+        } else {
+            let count = alphabet.indexOf(chr.toUpperCase());
+            let count2 = count + 13;
+            //Alphabet only has so many characters. Wrap around so that we arent grabbing undefined values
+            if (count2 >= alphabet.length) {
+                count2 -= 26;
+            }
+            answer += alphabet[count2];
+        }
+    }
+  return answer;
+}
+
+// Caesar Decipher
+function decipher(msg, code) {
+  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+  var answer = '';
+  for (var x = 0; x < msg.length; x++) {
+    var chr = msg[x];
+    var count = alphabet.indexOf(chr.toUpperCase());
+    var count2 = count + code;
+    if (chr == ' ') {
+    answer += ' ';
+    }
+    else if (count2 >= alphabet.length) {
+        count2 -= 26;
+    }
+    answer += alphabet[count2];
+  }
+  return answer;
 }
