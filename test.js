72d1ad37c85a11fbfd01f6eb2487653e7e2e5a70
Jessica Robinson <jessica.polansky@gmail.com>
2018-04-14 17:07:17 +0000

    Added additional answers to test file

--- test.js
+++ test.js
@@ -68,3 +68,30 @@ function printBanner(text) {
     console.log("*" + text + "*");
     console.log(Array(width + 3).join("*"));
 }
+
+function leetSpeak(entry) {
+    var upper = entry.toUpperCase();
+    var input = upper.split("");
+    var leet = []
+    for (var count = 0; count < input.length; count++) {
+        if (input[count] == "A") {
+            leet.push("4");
+        } else if (input[count] == "E") {
+            leet.push("3");
+        } else if (input[count] == "G") {
+            leet.push("6");
+        } else if (input[count] == "I") {
+            leet.push("1");
+        } else if (input[count] == "O") {
+            leet.push("0");
+        } else if (input[count] == "S") {
+            leet.push("5");
+        } else if (input[count] == "T") {
+            leet.push("7");
+        } else {
+            leet.push(input[count]);
+        }
+    }
+    var answer = leet.join('');
+    console.log(answer);
+}
